0c8a990c0bf98f495a24f953345bfde41a6f0ea3
sunhonglin <sunhonglin2018@yeah.net>
2021-05-07 10:30:03 +0800

    java dir convert to kotlin

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,12 +1,3 @@
-object Versions {
-    const val targetSdk = 29
-    const val minSdk = 23
-    const val archCore = "2.1.0"
-    const val appcompat = "1.1.0"
-    const val annotations = "16.0.1"
-    const val androidxCore = "1.2.0"
-}
-
 object Dependencies {
     object Android {
         const val gradlePlugin = "com.android.tools.build:gradle:_"
---
+++ buildSrc/src/main/kotlin/Version.kt
@@ -0,0 +1,8 @@
+object Versions {
+    const val targetSdk = 29
+    const val minSdk = 23
+    const val archCore = "2.1.0"
+    const val appcompat = "1.1.0"
+    const val annotations = "16.0.1"
+    const val androidxCore = "1.2.0"
+}
