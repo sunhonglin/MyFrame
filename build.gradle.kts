475b4e1bea262b747f239a6ec8194ebdbaaafd7c
sunhonglin <sunhonglin2018@yeah.net>
2022-11-26 11:28:32 +0800

    删除无用repositories

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,6 @@ buildscript {
         google()
         mavenCentral()
         gradlePluginPortal()
-        maven(url = Repositories.REPO_MAVEN_LANGCOO)
     }
 
     dependencies {
@@ -18,54 +17,15 @@ buildscript {
     }
 }
 
-//val kotlinVersion = dependenciesVersion(rootProject, "version.kotlin").toString()
-//val coroutinesCore = dependenciesUrl(rootProject, Dependencies.KotlinX.Coroutines.core, "version.kotlinx.coroutines")
-//val coroutinesAndroid = dependenciesUrl(rootProject, Dependencies.KotlinX.Coroutines.android, "version.kotlinx.coroutines")
-//val lifecycleLiveData = dependenciesUrl(rootProject, "androidx.lifecycle:lifecycle-livedata:", "version.androidx.lifecycle")
-//val lifecycleRuntime = dependenciesUrl(rootProject, "androidx.lifecycle:lifecycle-runtime:", "version.androidx.lifecycle")
-//val lifecycleViewModel = dependenciesUrl(rootProject, "androidx.lifecycle:lifecycle-viewmodel:", "version.androidx.lifecycle")
-//val androidxCore = dependenciesUrl(rootProject, "androidx.core:core:", Versions.androidxCore)
-//val appcompat = dependenciesUrl(rootProject, "androidx.appcompat:appcompat:",Versions.appcompat)
-//val annotations = dependenciesUrl(rootProject, "org.jetbrains:annotations:", Versions.annotations)
-//val coreCommon = dependenciesUrl(rootProject, "androidx.arch.core:core-common:", Versions.archCore)
-//val coreRuntime = dependenciesUrl(rootProject, "androidx.arch.core:core-runtime:", Versions.archCore)
-//
 subprojects {
     repositories {
         google()
         mavenCentral()
-        gradlePluginPortal()
         maven(url = Repositories.REPO_MAVEN_LANGCOO)
     }
 
     buildDir = File(rootProject.buildDir, name)
     group = property("app.group").toString()
-
-    afterEvaluate {
-        configurations.configureEach {
-//            // There could be transitive dependencies in tests with a lower version. This could cause
-//            // problems with a newer Kotlin version that we use.
-//            resolutionStrategy.force(Dependencies.Kotlin.reflect(kotlinVersion))
-//            resolutionStrategy.force(Dependencies.Kotlin.Stdlib.common(kotlinVersion))
-//            resolutionStrategy.force(Dependencies.Kotlin.Stdlib.jdk8(kotlinVersion))
-//            resolutionStrategy.force(Dependencies.Kotlin.Stdlib.jdk7(kotlinVersion))
-//            resolutionStrategy.force(Dependencies.Kotlin.Stdlib.jdk6(kotlinVersion))
-//
-//            resolutionStrategy.force(coroutinesCore)
-//            resolutionStrategy.force(coroutinesAndroid)
-//            resolutionStrategy.force(gson)
-//
-//            resolutionStrategy.force(lifecycleLiveData)
-//            resolutionStrategy.force(lifecycleRuntime)
-//            resolutionStrategy.force(lifecycleViewModel)
-//
-//            resolutionStrategy.force(androidxCore)
-//            resolutionStrategy.force(appcompat)
-//            resolutionStrategy.force(annotations)
-//            resolutionStrategy.force(coreCommon)
-//            resolutionStrategy.force(coreRuntime)
-        }
-    }
 }
 
 tasks.register<Delete>("clean") {
