46fc8873b97676e2b1c21f5c77e04a5d7311088a
sunhonglin <sunhonglin2018@yeah.net>
2023-07-24 17:20:11 +0800

    升级

--- app/src/main/kotlin/com/sunhonglin/myframe/data/login/LoginDS.kt
+++ app/src/main/kotlin/com/sunhonglin/myframe/data/login/LoginDS.kt
@@ -5,17 +5,12 @@ import com.sunhonglin.core.util.decodeFromString
 import com.sunhonglin.core.util.encodeToString
 import com.sunhonglin.myframe.data.login.model.LoginData
 
-class LoginDS {
-    companion object {
-        private const val LOGIN_INFO = "loginInfo"
+object LoginDS {
+    private const val LOGIN_INFO = "loginInfo"
 
-        fun saveLoginData(loginData: LoginData) {
-            DataStoreUtil.putSyncData(LOGIN_INFO, loginData.encodeToString())
-        }
+    val loginData
+        get() = LoginData.serializer().decodeFromString(DataStoreUtil.getSyncData(LOGIN_INFO, ""))
 
-        fun getLoginData(): LoginData? {
-            return LoginData.serializer()
-                .decodeFromString(DataStoreUtil.getSyncData(LOGIN_INFO, ""))
-        }
-    }
+    fun saveLoginData(loginData: LoginData) =
+        DataStoreUtil.putSyncData(LOGIN_INFO, loginData.encodeToString())
 }
